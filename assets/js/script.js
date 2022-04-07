7c8b497a670a8fbfa5e13d5360e4e4020e683a3d
iggy <allyson.nostrandd@gmail.com>
2022-04-06 22:38:15 -0700

    working on getting userinput to stay on the page when page reloads

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
-var userData = document.getElementById('userInput')
-
-
+var allUserData =[]
+/*Displays the date in jumbotron----------------------------------------*/
 function displayDate (){
 var currentDate= moment().format("MMM Do, YYYY");
 $("#today-date").text(currentDate)
@@ -8,7 +7,21 @@ $("#today-date").text(currentDate)
 
 setInterval(displayDate,1000)
 
+
+/*save btn function-----------------------------------------------------*/
  $(".saveBtn").on("click", function(){
-    userData.
+    var userData = $(this).siblings("textarea").val()
+    localStorage.setItem("userData", userData)
+    console.log(userData)
+    UserData = JSON.stringify(userData)
+    allUserData.push(userData)
+    localStorage.setItem("allUserData", allUserData)
+    localStorage.getItem("userData", userData)
+    console.log(allUserData)
+
+    for ( var i = 0; i < allUserData.length; i++){
+
+    }
  })
 
+/*refresh page doesnt get rid of text area content--------------------*/
