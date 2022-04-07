86d95e57353f8d3ce1ca824751ef9a5a3fd7f40b
iggy <allyson.nostrandd@gmail.com>
2022-04-06 19:47:30 -0700

    working out function of save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,14 @@
+var userData = document.getElementById('userInput')
+
+
+function displayDate (){
 var currentDate= moment().format("MMM Do, YYYY");
 $("#today-date").text(currentDate)
+}
+
+setInterval(displayDate,1000)
+
+ $(".saveBtn").on("click", function(){
+    userData.
+ })
+
