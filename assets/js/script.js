c9f401876754023dd496c198b8a8b4673b7aa3c7
iggy <allyson.nostrandd@gmail.com>
2022-04-07 19:15:27 -0700

    user input gets saved to the page now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+const textArea = document.getElementById("#userInput")
+
 var allUserData =[]
 /*Displays the date in jumbotron----------------------------------------*/
 function displayDate (){
@@ -11,17 +13,24 @@ setInterval(displayDate,1000)
 /*save btn function-----------------------------------------------------*/
  $(".saveBtn").on("click", function(){
     var userData = $(this).siblings("textarea").val()
-    localStorage.setItem("userData", userData)
-    console.log(userData)
-    UserData = JSON.stringify(userData)
-    allUserData.push(userData)
-    localStorage.setItem("allUserData", allUserData)
-    localStorage.getItem("userData", userData)
-    console.log(allUserData)
+    var time = $(this).parent().attr("id")
+    localStorage.setItem(time, userData)
+    // console.log(userData)
+    // UserData = JSON.stringify(userData)
+    // allUserData.push(userData)
+    // localStorage.setItem("allUserData", allUserData)
+    // localStorage.getItem("userData", userData)
 
-    for ( var i = 0; i < allUserData.length; i++){
+    // var test = document.createElement("h3")
 
-    }
+
  })
 
 /*refresh page doesnt get rid of text area content--------------------*/
+
+// $("textarea").val(localStorage.getItem(allUserData))
+
+
+$(document).ready(function(){
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+})
